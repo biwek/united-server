efd26b8abd334791e50fb98b5b88a53dd2be96fb
Biwek Shrestha <biwekshrestha@biweks-mbp.default>
2014-04-04 00:09:11 +0100

    added rails and changed db to postgres

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 United App Server
-=======
+=====
 
 Server for united application
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UnitedServer::Application.config.secret_key_base = 'cf1a842a5362c053f9d41934928f2603af160036751a6baae7668f02d862f6340a38a3ddca0bba4449102acfd4b715f0c6b3f21a6bfb4436f9f0e10d6a1ef9e5'
