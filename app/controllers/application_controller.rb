48b192059e330a83e2281acc8a26ce3f2380e5ec
Biwek Shrestha <biwekshrestha@biweks-mbp.default>
2014-04-09 13:12:50 +0100

    removed current user test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :current_user, :cors_preflight_check
+  before_filter :cors_preflight_check
   after_filter :cors_set_access_control_headers
 
   def cors_set_access_control_headers
